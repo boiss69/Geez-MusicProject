454b5a69baa2b0c50f8366a9c99da5ac90aac591
Anonym <78007404+boiss69@users.noreply.github.com>
2021-07-25 07:47:53 +0700

    Update msg.py

--- GeezProject/modules/msg.py
+++ GeezProject/modules/msg.py
@@ -20,11 +20,11 @@ class Messages():
       HELP_MSG = [
         ".",
 f"""
-**Hey 👋 Selamat datang kembali di {PROJECT_NAME}
+Hey 👋 Selamat datang kembali di {PROJECT_NAME}
 
 ✣️ {PROJECT_NAME} dapat Memutar Lagu di Voice Chat Group Dengan cara yang Mudah.
 
-✣️ Assistant Music » @{ASSISTANT_NAME}\n\nKlik Next untuk instruksi**
+✣️ Assistant Music » @{ASSISTANT_NAME}\n\nKlik Next untuk instruksi
 
 """,
 
@@ -32,10 +32,13 @@ f"""
 **Pengaturan**
 
 1. Jadikan bot sebagai admin
-2. Mulai obrolan suara / VCG
+2. Mulai obrolan suara atau vcg
 3. Ketik `/userbotjoin` dan coba /play <nama lagu>
 × Jika Assistant Bot bergabung selamat menikmati musik, 
 × Jika Assistant Bot tidak bergabung Silahkan Tambahkan @{ASSISTANT_NAME} ke grup Anda dan coba lagi
+× Jika Assistant Bot masih tidak dapat bergabung, berarti bot sudah full grupchat silahkan gunakan bot lain.
+
+More info silahkan cek channel @boimusicnetwork.
 
 
 **» Perintah Untuk dalam grup Member Juga Bisa :**
